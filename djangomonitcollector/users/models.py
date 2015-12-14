f6f63509786f6f99f0cf2c109a17c88975c0d981
Meher gara <meher.gara@gmail.com>
2015-12-14 08:31:04 -0500

    Removed unused models

--- djangomonitcollector/users/models.py
+++ djangomonitcollector/users/models.py
@@ -14,9 +14,16 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    is_customer = models.BooleanField(blank=True,default=True)
+
 
     def __str__(self):
         return self.username
 
     def get_absolute_url(self):
         return reverse('users:detail', kwargs={'username': self.username})
+
+def validate_user(user):
+    if user.is_customer and user.is_active:
+        return True
+    return False
