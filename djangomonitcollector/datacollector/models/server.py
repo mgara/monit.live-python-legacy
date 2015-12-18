ce11961740ba41e9334436fd8bce9bc038154841
Meher gara <meher.gara@gmail.com>
2015-12-18 12:30:01 -0500

    Added extra pameters for server definition/configuration (Timezone/http_port/http_url/monit_username/monit_password)

--- djangomonitcollector/datacollector/models/server.py
+++ djangomonitcollector/datacollector/models/server.py
@@ -1,4 +1,5 @@
 from xml.dom import minidom
+import sys
 
 from django.db import models
 
@@ -10,54 +11,71 @@ from platform import Platform
 from file import File
 from url import Host
 from program import Program
-from utils import remove_old_services, get_value
-import sys
+from utils import remove_old_services, get_value,TIMEZONES_CHOICES
+from djangomonitcollector.users.models import CollectorKey,User
+
+
+class CollectorKeyError(Exception):
+    def __init__(self, message):
+        self.message = message
 
 
 class Server(models.Model):
     monit_id = models.CharField(max_length=32, unique=True)
+    user_id = models.ForeignKey(User)
+
     monit_version = models.TextField(null=True)
     localhostname = models.TextField(null=True)
     uptime = models.IntegerField(null=True)
     address = models.TextField(null=True)
 
+    http_address = models.CharField(max_length=200,null=True)
+    http_username = models.CharField(max_length=45,default="monit")
+    http_password = models.CharField(max_length=45,default="admin")
+    monit_update_period = models.IntegerField(default=60)
+    data_timezone = models.CharField(max_length=30,choices=TIMEZONES_CHOICES, default='America/Montreal')
+
+    is_new = models.BooleanField(default=True)
+
     @classmethod
-    def update(cls, xmldoc, monit_id):
+    def update(cls, xmldoc, monit_id, user):
         reporting_services = []
-        server, created = cls.objects.get_or_create(monit_id=monit_id)
+        server, created = cls.objects.get_or_create(monit_id=monit_id, user_id=user)
         server.monit_version = xmldoc.getElementsByTagName(
             'monit')[0].attributes["version"].value
         server.localhostname = get_value(xmldoc, "localhostname", "")
         server.uptime = get_value(xmldoc, "server", "uptime")
         server.address = get_value(xmldoc, "server", "address")
         server.save()
+
         Platform.update(xmldoc, server)
-        for service in xmldoc.getElementsByTagName('services')[0].getElementsByTagName('service'):
-            service_type = get_value(service, "type", "")
-            service_name = get_value(service, "", "", "name")
-            reporting_services.append(service_name)
+        if not server.is_new:
+            for service in xmldoc.getElementsByTagName('services')[0].getElementsByTagName('service'):
+                service_type = get_value(service, "type", "")
+                service_name = get_value(service, "", "", "name")
+                reporting_services.append(service_name)
 
-            if service_type == '0':  # Filesystem
-                FileSystem.update(xmldoc, server, service)
-            elif service_type == '2':  # File
-                File.update(xmldoc, server, service)
-            elif service_type == '3':  # Process
-                Process.update(xmldoc, server, service)
-            elif service_type == '4':  # Host
-                Host.update(xmldoc, server, service)
-            elif service_type == '5':  # System Analysis
-                System.update(xmldoc, server, service)
-            elif service_type == '7':  # Program
-                Program.update(xmldoc, server, service)
-            elif service_type == '8':  # Network Card
-                Net.update(xmldoc, server, service)
-            else:
-                Process.update(xmldoc, server, service)
-        remove_old_services(server, reporting_services)
+                if service_type == '0':  # Filesystem
+                    FileSystem.update(xmldoc, server, service)
+                elif service_type == '2':  # File
+                    File.update(xmldoc, server, service)
+                elif service_type == '3':  # Process
+                    Process.update(xmldoc, server, service)
+                elif service_type == '4':  # Host
+                    Host.update(xmldoc, server, service)
+                elif service_type == '5':  # System Analysis
+                    System.update(xmldoc, server, service)
+                elif service_type == '7':  # Program
+                    Program.update(xmldoc, server, service)
+                elif service_type == '8':  # Network Card
+                    Net.update(xmldoc, server, service)
+                else:
+                    Process.update(xmldoc, server, service)
+            remove_old_services(server, reporting_services)
+        else:
+            raise StandardError("New Server, you must configure it in your server settings")
 
-
-def collect_data(xml_str):
-    # only ready data if it has a monit id
+def collect_data(xml_str, ck):
     try:
         xmldoc = minidom.parseString(xml_str)
         monit_id = xmldoc.getElementsByTagName(
@@ -65,8 +83,14 @@ def collect_data(xml_str):
     except:
         return False, "Problem parsing the xml document"
     try:
-        Server.update(xmldoc, monit_id)
-    except:
-        e = sys.exc_info()[0]
-        return False, "Error While updating the server instance: {0}".format(e)
+        ckobj = CollectorKey.objects.get(pk=ck)
+        if ckobj:
+            if ckobj.is_enabled:
+                Server.update(xmldoc, monit_id, ckobj.user_id)
+            else:
+                raise CollectorKeyError("Key Not Active {0}".format(ck))
+        else:
+            raise CollectorKeyError("No Such Key Error {0}".format(ck))
+    except StandardError as e:
+        return False, "Error While updating the server instance: {0}".format(e.message)
     return True, "Server instance Updated"
