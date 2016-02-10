da14b147e18558606ba38de34d1b91ceda9858da
Meher gara <meher.gara@gmail.com>
2016-02-10 08:53:51 -0500

    Updated file model to save more details : permission, gid, uid, mode

--- djangomonitcollector/datacollector/models/file.py
+++ djangomonitcollector/datacollector/models/file.py
@@ -1,11 +1,16 @@
 from django.db import models
 
 from service import Service
-from utils import get_value
+from utils import get_value, get_int
 
 
 class File(Service):
     server = models.ForeignKey('Server')
+    size = models.PositiveIntegerField(null=True)
+    permission = models.CharField(null=True ,max_length=4)
+    uid = models.IntegerField(null=True)
+    gid = models.IntegerField(null=True)
+    timestamp = models.CharField(null=True,max_length=20)
 
     @classmethod
     def update(cls, xmldoc, server, service):
@@ -15,8 +20,16 @@ class File(Service):
         file_.status = get_value(service, "status", "")
         file_.status_hint = get_value(service, "status_hint", "")
         file_.monitor = get_value(service, "monitor", "")
-        file_.monitormode = get_value(service, "monitormode", "")
-        file_.pendingaction = get_value(service, "pendingaction", "")
+        file_.service_type = get_value(service, "type", "")
+        file_.monitor_mode = get_value(service, "monitormode", "")
+        file_.pending_action = get_value(service, "pendingaction", "")
         file_.s_id = file_.server.id
+
+        file_.size = get_int(service, "size", "")
+        file_.permission = get_value(service, "mode", "")
+        file_.uid = get_int(service, "uid", "")
+        file_.gid = get_int(service, "gid", "")
+        file_.timestamp = get_value(service, "timestamp", "")
+
         file_.save()
         return file
