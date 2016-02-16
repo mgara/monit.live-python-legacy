9a236b1fee74be4c8f019788b4ef6994c5c8d4ab
meher.gara <meher.gara@gmail.com>
2016-02-16 17:04:24 -0500

    Added post class to post event json to any given URL

---
+++ djangomonitcollector/notificationsystem/lib/posteventnotification.py
@@ -0,0 +1,29 @@
+__author__ = 'mehergara'
+
+from httplib import HTTPConnection
+from urlparse import urlparse
+
+from ieventnotification import IEventSettingsInterface
+from parameter import Parameter
+
+
+class PostEventNotification(IEventSettingsInterface):
+    extra_params = {
+        'url': Parameter('url', 'URL'),
+    }
+
+    def process(self):
+        url = self.extra_params['url']
+        self.post(url, self.get_event_summary())
+
+    def finalize(self, event_object):
+        # put whatever you want to be done after the process command
+        pass
+
+    def post(settings, url, data):
+        urlparts = urlparse(url)
+        conn = HTTPConnection(urlparts.netloc, urlparts.port)
+        conn.request("POST", urlparts.path, data)
+        resp = conn.getresponse()
+        body = resp.read()
+        print body
