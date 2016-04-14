9a3723ee50149f56c3ac8a88bc52a6c0f71ebab7
meher.gara <meher.gara@gmail.com>
2016-04-14 14:02:03 -0400

    Removed new relic references from the wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangomonitcollector project.
+WSGI config for toto project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
@@ -15,13 +15,10 @@ framework.
 """
 import os
 
-if os.environ.get("DJANGO_SETTINGS_MODULE") == "config.settings.production":
-    import newrelic.agent
-    newrelic.agent.initialize()
+
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-if os.environ.get("DJANGO_SETTINGS_MODULE") == "config.settings.production":
-    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+
+
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
@@ -34,13 +31,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 # setting points here.
 application = get_wsgi_application()
 
-# Use Whitenoise to serve static files
-# See: https://whitenoise.readthedocs.org/
-application = DjangoWhiteNoise(application)
-if os.environ.get("DJANGO_SETTINGS_MODULE") == "config.settings.production":
-    application = Sentry(application)
-if os.environ.get("DJANGO_SETTINGS_MODULE") == "config.settings.production":
-    application = newrelic.agent.WSGIApplicationWrapper(application)
+
+
+
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
