83d3e0c4eeef041bc7b30127b045e3e5a127e0f6
Meher Gara <meher.gara@vantrix.com>
2016-04-22 14:24:53 -0400

    Added supervisor required files Added monit-collector.ini for supervisor Added heartbeat.sh as a script to start the monit heartbeat

--- compose/django/heartbeat.sh
+++ compose/django/heartbeat.sh
@@ -4,5 +4,4 @@ source /opt/monit-collector/venv/collector/bin/activate
 source /opt/monit-collector/app/django-monit-collector/source_me
 cd /opt/monit-collector/app/django-monit-collector
 
-python manage.py collectstatic --noinput
-gunicorn config.wsgi -w 4 -b 127.0.0.1:5000 --chdir=.
+python manage.py  host_heartbeat --host localhost --port 5000
