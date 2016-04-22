c182ba9cab5421b75624d78fc1bc6550a8f391d2
Meher Gara <meher.gara@vantrix.com>
2016-04-22 14:45:14 -0400

    Fixed path for the applications

--- compose/django/gunicorn.sh
+++ compose/django/gunicorn.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-source /opt/monit-collector/venv/collector/bin/activate
-source /opt/monit-collector/app/django-monit-collector/source_me
-cd /opt/monit-collector/app/django-monit-collector
+source /opt/monit-collector/venv/bin/activate
+source /opt/monit-collector/app/source_me
+cd /opt/monit-collector/app
 
 python manage.py collectstatic --noinput
 gunicorn config.wsgi -w 4 -b 127.0.0.1:5000 --chdir=.
--- compose/django/heartbeat.sh
+++ compose/django/heartbeat.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-source /opt/monit-collector/venv/collector/bin/activate
-source /opt/monit-collector/app/django-monit-collector/source_me
-cd /opt/monit-collector/app/django-monit-collector
+source /opt/monit-collector/venv/bin/activate
+source /opt/monit-collector/app/source_me
+cd /opt/monit-collector/app
 
 python manage.py  host_heartbeat --host localhost --port 5000
