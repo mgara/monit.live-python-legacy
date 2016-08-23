85ed9a6967fb93142ae75f1d145fde3b45e443c5
Meher Gara <meher.gara@gmail.com>
2016-08-23 16:51:12 -0400

    Started working on save widget state

--- djangomonitcollector/static/js/server.js
+++ djangomonitcollector/static/js/server.js
@@ -1,17 +1,17 @@
-    var minimize=function(btn){
-                    header = $(btn).parent()
+    var minimize = function(btn, url) {
+        header = $(btn).parent()
 
-                    title = header.children('h2')
-                    card = header.parent()
-                    card_body = card.children('div.card-body')
-                    card_body.toggle(300)
-                    if (card_body.hasClass("toggled")){
-                        $(btn).html('<i class="zmdi zmdi-minus"></i>')
-                        card_body.removeClass("toggled")
-                        title.fadeTo( "slow" , 1)
-                    }else{
-                        $(btn).html('<i class="zmdi zmdi-plus"></i>')
-                        card_body.addClass("toggled")
-                        title.fadeTo( "slow" , 0.4)
-                    }
-                }
+        title = header.children('h2')
+        card = header.parent()
+        card_body = card.children('div.card-body')
+        card_body.toggle(300)
+        if (card_body.hasClass("toggled")) {
+            $(btn).html('<i class="zmdi zmdi-minus"></i>')
+            card_body.removeClass("toggled")
+            title.fadeTo("slow", 1)
+        } else {
+            $(btn).html('<i class="zmdi zmdi-plus"></i>')
+            card_body.addClass("toggled")
+            title.fadeTo("slow", 0.4)
+        }
+    }
