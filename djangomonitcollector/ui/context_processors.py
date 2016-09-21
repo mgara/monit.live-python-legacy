f8af463db4d95e0bd628cc28fdb3f8b3dd9e682d
root <root@kairos.localdomain>
2016-09-21 08:45:13 -0400

    Added context processor for the global env variables Added event notification row template

---
+++ djangomonitcollector/ui/context_processors.py
@@ -0,0 +1,20 @@
+from djangomonitcollector.datacollector.models import MonitEvent
+
+from django.conf import settings # import the settings file
+
+
+def app_settings(request):
+
+    # return the value you want as a dictionnary. you may add multiple values in there.
+    user = request.user
+    organisation_events = None
+
+    if user.is_authenticated():
+        org = user.organisation
+        organisation_events = MonitEvent.objects.filter(server__organisation=org).order_by('-id')[:10]
+
+    return {
+        'APPNAME': settings.APPNAME,
+        'APPVERSION': settings.APPVERSION,
+        'organisation_events': organisation_events
+    }
